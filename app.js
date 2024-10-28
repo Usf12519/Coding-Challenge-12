715b7be838b717d20a5c97b7183ebe237b1336cc
Cameron Smith <Cs11@usf.edu>
2024-10-27 22:55:07 -0400

    Setup HTML structure for the drawing application.

---
+++ app.js
@@ -0,0 +1,25 @@
+// Task 1: Set Up the HTML Structure
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Canvas Drawing App</title>
+</style>
+<label>
+    <input type="radio" name="tool" value="paper" checked>paper
+</label>
+</label>
+    <input type="radio" name="tool" value="puck" checked>puck
+</label>
+
+<br><br>
+
+<label for="color">Select Color:</label>
+<input type="color" id="color" value="1234567">
+
+<br><br>
+
+<button id="clearcanvas">Clear Canvas</button>
+
